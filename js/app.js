209e29b3225153b1f1819ce0c9bee453c7156015
cmulcunry <89563322+cmulcunry@users.noreply.github.com>
2021-11-30 15:38:26 -0500

    added event listener for left button

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ window.onload = () => {
     let right = document.getElementById(`right`);
     let slides = document.querySelectorAll(`main p`);
     let currentSlide = 0;
+    slides[0].style.display = `block`;
 
     right.addEventListener(`click`, () => {
     currentSlide = currentSlide + 1;
@@ -24,7 +25,35 @@ window.onload = () => {
         slides[2].style.display = `none`;
         slides[3].style.display = `block`;
     }
-    else if (currenSlide === 0) {
+    else if (currentSlide === 0) {
+        slides[0].style.display = `block`;
+        slides[1].style.display = `none`;
+        slides[2].style.display = `none`;
+        slides[3].style.display = `none`;
+    }
+    } )
+
+    left.addEventListener(`click`, () => {
+    currentSlide = currentSlide - 1;
+    if (currentSlide === 1)  {
+        slides[0].style.display = `none`;
+        slides[1].style.display = `block`;
+        slides[2].style.display = `none`;
+        slides[3].style.display = `none`;
+    }
+    else if (currentSlide === 2) {
+        slides[0].style.display = `none`;
+        slides[1].style.display = `none`;
+        slides[2].style.display = `block`;
+        slides[3].style.display = `none`;
+    }
+    else if (currentSlide === 3) {
+        slides[0].style.display = `none`;
+        slides[1].style.display = `none`;
+        slides[2].style.display = `none`;
+        slides[3].style.display = `block`;
+    }
+    else if (currentSlide === 0) {
         slides[0].style.display = `block`;
         slides[1].style.display = `none`;
         slides[2].style.display = `none`;
